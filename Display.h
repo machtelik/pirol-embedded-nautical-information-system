e2c9ae96f9b294ccf39b85d6100127b51394189c
Mike Achtelik <mike.achtelik@gmail.com>
2023-11-24 23:19:10 +0100

    Add some more display stuff

---
+++ Display.h
@@ -0,0 +1,96 @@
+#include <LiquidCrystal_I2C.h>
+
+#define DISPLAY_ADDRESS_0 0x22
+#define DISPLAY_ADDRESS_1 0x23
+#define DISPLAY_ADDRESS_2 0x25
+#define DISPLAY_ADDRESS_3 0x26
+#define DISPLAY_ADDRESS_4 0x27
+
+#define DISPLAY_COUNT 5
+#define DISPLAY_LINE_COUNT 4
+#define DISPLAY_CHAR_COUNT 20
+#define DISPLAY_REFRESH_PERIODE 5000
+
+class Display
+{
+public:
+    Display(uint8_t addr) : lcd(addr, DISPLAY_CHAR_COUNT, DISPLAY_LINE_COUNT),
+                            lastUpdate(0),
+                            needsRefresh(true),
+                            lines()
+    {
+    }
+
+    void setup()
+    {
+        lcd.init();
+        lcd.backlight();
+    }
+
+    void refresh()
+    {
+        if (!needsRefresh)
+        {
+            return;
+        }
+
+        if (millis() - lastUpdate < DISPLAY_REFRESH_PERIODE)
+        {
+            return;
+        }
+
+        for (int i = 0; i < DISPLAY_LINE_COUNT; ++i)
+        {
+            lcd.setCursor(0, i);
+            lcd.print(lines[i]);
+        }
+
+        lastUpdate = millis();
+        needsRefresh = false;
+    }
+
+    void setText(int i, const String &line)
+    {
+        if (i < 0 || i >= DISPLAY_LINE_COUNT)
+        {
+            return;
+        }
+
+        if (line.length() > DISPLAY_CHAR_COUNT)
+        {
+            return;
+        }
+
+        lines[i] = line;
+        needsRefresh = true;
+    }
+
+private:
+    LiquidCrystal_I2C lcd;
+    unsigned long lastUpdate;
+    bool needsRefresh;
+    String lines[4];
+};
+
+Display displays[] = {
+    Display(DISPLAY_ADDRESS_0),
+    Display(DISPLAY_ADDRESS_1),
+    Display(DISPLAY_ADDRESS_2),
+    Display(DISPLAY_ADDRESS_3),
+    Display(DISPLAY_ADDRESS_4)};
+
+void setupDisplays()
+{
+  for (int i = 0; i < DISPLAY_COUNT; ++i)
+  {
+    displays[i].setup();
+  }
+}
+
+void refreshDisplays()
+{
+  for (int i = 0; i < DISPLAY_COUNT; ++i)
+  {
+    displays[i].refresh();
+  }
+}
--- pirol-embedded-nautical-information-system.ino
+++ pirol-embedded-nautical-information-system.ino
@@ -1,68 +1,6 @@
-#include <LiquidCrystal_I2C.h>
+#include "Display.h"
 
-#define DISPLAY_ADDRESS_0 0x22
-#define DISPLAY_ADDRESS_1 0x23
-#define DISPLAY_ADDRESS_2 0x25
-#define DISPLAY_ADDRESS_3 0x26
-#define DISPLAY_ADDRESS_4 0x27
 
-#define DISPLAY_COUNT 5
-#define DISPLAY_REFRESH_PERIODE 5000
-
-class Display
-{
-public:
-  Display(uint8_t addr) : lcd(addr, 20, 4),
-                          lastUpdate(0),
-                          needsRefresh(true),
-                          line()
-  {
-  }
-
-  void setup()
-  {
-    lcd.init();
-    lcd.backlight();
-  }
-
-  void refresh()
-  {
-    if (!needsRefresh)
-    {
-      return;
-    }
-
-    if (millis() - lastUpdate < DISPLAY_REFRESH_PERIODE)
-    {
-      return;
-    }
-
-    lcd.setCursor(0, 0);
-    lcd.print(line);
-
-    lastUpdate = millis();
-    needsRefresh = false;
-  }
-
-  void setText(const String &line)
-  {
-    line = line;
-    needsRefresh = true;
-  }
-
-private:
-  LiquidCrystal_I2C lcd;
-  unsigned long lastUpdate;
-  bool needsRefresh;
-  String line;
-};
-
-Display displays[] = {
-    Display(DISPLAY_ADDRESS_0),
-    Display(DISPLAY_ADDRESS_1),
-    Display(DISPLAY_ADDRESS_2),
-    Display(DISPLAY_ADDRESS_3),
-    Display(DISPLAY_ADDRESS_4)};
 
 void setup()
 {
@@ -70,25 +8,38 @@ void setup()
 
   delay(500);
 
-  for (int i = 0; i < DISPLAY_COUNT; ++i)
-  {
-    displays[i].setup();
-  }
+  setupDisplays();
 
   // Switch to I2C fast mode
   Wire.setClock(400000);
 
-  displays[0].setText("A");
-  displays[1].setText("B");
-  displays[2].setText("C");
-  displays[3].setText("1234567890 abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-  displays[4].setText("Hi? Ho! 2^3 30 C 123 RPM");
+  displays[0].setText(0, "Kuehlwasser:     86C");
+  displays[0].setText(1, "Motor Gehaeuse:  42C");
+  displays[0].setText(2, "Motoroel:        65C");
+  displays[0].setText(3, "Umgebung:        30C");
+
+  displays[1].setText(0, "Luft:            22C");
+  displays[1].setText(1, "Wasser:          12C");
+  displays[1].setText(2, "Feuchte:         62%");
+  displays[1].setText(3, "Druck:      1001 hPa");
+  
+  displays[2].setText(0, "Battery A:     11.4V");
+  displays[2].setText(1, "Battery B:     13.4V");
+  displays[2].setText(2, "Verbrauch:     20.4W");
+  displays[2].setText(3, "Landstrom:        0V");
+  
+  displays[3].setText(0, "Diesel:          33%");
+  displays[3].setText(1, "Wasser:          92%");
+  displays[3].setText(2, "Abwasser:         2%");
+  displays[3].setText(3, "Bier:          3/120");
+  
+  displays[4].setText(0, "Was ist rot und");
+  displays[4].setText(1, "wiehert?");
+  displays[4].setText(2, "--------------------");
+  displays[4].setText(3, "Eine Pferdbeere!");
 }
 
 void loop()
 {
-  for (int i = 0; i < DISPLAY_COUNT; ++i)
-  {
-    displays[i].refresh();
-  }
+  refreshDisplays();
 }
